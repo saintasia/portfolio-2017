74c0bda99d9ba7ab0d070f3be26e387a9f52f8d9
Anastasia Kashkinova <anastasia.kashkinova@gmail.com>
2017-10-09 00:20:39 +0900

    first commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+//modalbox
+var modalBox = document.querySelector('.modal');
+
+modalBox.addEventListener('click', function(event){
+   event.preventDefault();
+   if (this.classList.contains('mod-open')){
+   this.classList.remove('mod-open');
+    }
+});
+
+//angular
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope){
+    $scope.works = [
+        {
+            "Image" : "img/burger-cafe.jpg",
+            "Name" : "Burger Joint landing page HTML template",
+            "TextEn" : "Simple customizable and responsive one-page html and css template for a burger joint or any other cafe and eatery.",
+            "TextJa" : "カフェや飲食店に使いやすいHTML5レスポンシブランディングページテンプレートのデザインとコーディング。",
+            "Tags" : "#css3 #html5 #sass #jquery #design #responsive",
+            "Link" : "https://www.astemplates.com/free-website-template/773-burger-cafe"     
+        },
+        {
+            "Image" : "img/shibuweb.jpg",
+            "Name" : "Shibuweb",
+            "TextEn" : "Shibuweb digital agency logo design, landing design & front end coding. (Top page only)",
+            "TextJa" : "Shibuweb デジタルエイジェンシーロゴ、トップページデザイン、およびトプページコーディング(トップページのみ)",
+            "Tags" : "#bootstrap #css3 #html5 #jquery #design #logo #svg #responsive",
+            "Link" : "http://shibuweb.com/"
+        },
+        {
+            "Image" : "img/chitose.jpg",
+            "Name" : "NEW CHITOSE ERENTAKA RENEWAL",
+            "TextEn" : "New Chitose Car Rental wordpress site redesign, frontend renewal and backend updates such as plugin customization, php error fixing, and responsive development.",
+            "TextJa" : "新千歳レンタカーWordPressホームページの再デザイン、バックエンド更新、プラグインのカスタマイズ、フロントエンドリニューアル、レスポンシブ対応追加。",
+            "Tags" : "#php #wordpress #css3 #html5 #jquery #design #responsive",
+            "Link" : "http://chitosekuukourentacar.com/"
+        },
+        {
+            "Image" : "img/consultant.jpg",
+            "Name" : "Excia consultant mobile app design",
+            "TextEn" : "Mobile app design with chat for getting estimate of a vehicle's value.",
+            "TextJa" : "チャットで中古車買取相談できるスマホアプリデザイン。",
+            "Tags" : "#ui #design #app",
+            "Link" : "https://marvelapp.com/30adf9a/"
+        }
+               
+    ];
+    $scope.openImg = function(){
+        imgSrc = this.work.Image;
+        modalBox.querySelector('img').src=imgSrc;
+        modalBox.classList.add('mod-open');
+    }
+});
